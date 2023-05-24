83eefae02c9d93d6c1fdb5c6637d4bfc6ea0fb24
emaicorp <64812109+emaicorp@users.noreply.github.com>
2023-05-24 19:51:16 +0100

    Update RoadMap.js
    added a road map card

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -1,21 +1,10 @@
 import React from "react";
-import ReactFlow from 'reactflow';
-
-import 'reactflow/dist/style.css';
-
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 export default function RoadMap(){
   return(
     <div>
-    <h1>hello world</h1>
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
+    <h1>Road Map</h1>
+    
     </div>
   );
 }
